4ccf9d6c4ff7a159544bcb25e0c45a14b2ad56a4
Uday03-tech <udaysawant6651@gmail.com>
2026-06-16 14:44:54 +0530

    First Commit

---
+++ spotify-clone/src/components/SongItem.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { PlayerContext } from "../context/PlayerContext";
+
+const SongItem = ({ name, image, desc, id }) => {
+  const { playWithId } = useContext(PlayerContext);
+
+  return (
+    <div
+      onClick={() => playWithId(id)}
+      className="min-w-[150px] max-w-[150px] p-2 rounded cursor-pointer hover:bg-[#ffffff26] flex-shrink-0"
+    >
+      <img
+        className="rounded w-[150px] h-[150px] object-cover"
+        src={image}
+        alt=""
+      />
+
+      <p className="font-semibold mt-2 text-sm truncate">{name}</p>
+
+      <p
+        className="text-slate-300 text-xs mt-1 overflow-hidden"
+        style={{
+          display: "-webkit-box",
+          WebkitLineClamp: 2,
+          WebkitBoxOrient: "vertical",
+        }}
+      >
+        {desc}
+      </p>
+    </div>
+  );
+};
+
+export default SongItem;
