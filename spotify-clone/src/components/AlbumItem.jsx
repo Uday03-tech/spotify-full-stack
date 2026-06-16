f21fbf8d45cdefad205808cfe7e9367faf150319
Uday03-tech <udaysawant6651@gmail.com>
2026-06-16 17:14:36 +0530

    Add Songs & Images

--- spotify-clone/src/components/AlbumItem.jsx
+++ spotify-clone/src/components/AlbumItem.jsx
@@ -7,17 +7,15 @@ const AlbumItem = ({ image, name, desc, id }) => {
   return (
     <div
       onClick={() => navigate(`/album/${id}`)}
-      className="min-w-[150px] max-w-[150px] p-2 rounded cursor-pointer hover:bg-[#ffffff26] flex-shrink-0"
+      className="min-w-[160px] max-w-[150px] p-2 rounded cursor-pointer hover:bg-[#ffffff26] flex-shrink-0"
     >
       <img
-        className="rounded w-[150px] h-[150px] object-cover"
+        className="rounded w-[160px] h-[150px] object-cover"
         src={image}
         alt=""
       />
 
-      <p className="font-semibold mt-2 text-sm truncate">
-        {name}
-      </p>
+      <p className="font-semibold mt-2 text-sm truncate">{name}</p>
 
       <p
         className="text-slate-300 text-xs mt-1 overflow-hidden"
--- spotify-clone/src/components/Sidebar.jsx
+++ spotify-clone/src/components/Sidebar.jsx
@@ -15,8 +15,8 @@ const Sidebar = () => {
           <img className="w-5" src={assets.home_icon} alt="" />
           <p className="font-bold">Home</p>
         </div>
-        <div className="flex items-center gap-3 pl-8 cursor-pointer">
-          <img className="w-6" src={assets.search_icon} alt="" />
+        <div className="flex items-center gap-3 pl-5 cursor-pointer">
+          <img className="w-5" src={assets.search_icon} alt="" />
           <p className="font-bold">Search</p>
         </div>
       </div>
@@ -34,14 +34,14 @@ const Sidebar = () => {
         <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
           <h1>Create your first playlist</h1>
           <p className="font-light">it's easy we will help you</p>
-          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-3">
             Create Playlist
           </button>
         </div>
         <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4">
           <h1>Let's findsome podcasts to follow</h1>
           <p className="font-light">we'll keep you update on new episodes</p>
-          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-3">
             Browse podcasts
           </button>
         </div>
