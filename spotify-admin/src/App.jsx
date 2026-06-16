06ea98359a3998b1299898d3aea6854081b0db9c
Uday03-tech <udaysawant6651@gmail.com>
2026-06-16 18:46:31 +0530

    Add .env file

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -1,23 +1,62 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import AddSong from './pages/AddSong/AddSong';
-import ListSong from './pages/ListSong/ListSong';
-import AddAlbum from './pages/AddAlbum/AddAlbum';
-import ListAlbum from './pages/ListAlbum/ListAlbum';
-import Sidebar from './components/Sidebar/Sidebar';
-import Navbar from './components/Navbar/Navbar';
+// import React from 'react'
+// import { Route, Routes } from 'react-router-dom';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import AddSong from './pages/AddSong/AddSong';
+// import ListSong from './pages/ListSong/ListSong';
+// import AddAlbum from './pages/AddAlbum/AddAlbum';
+// import ListAlbum from './pages/ListAlbum/ListAlbum';
+// import Sidebar from './components/Sidebar/Sidebar';
+// import Navbar from './components/Navbar/Navbar';
 
-export const url = 'http://localhost:4000';
+// export const url = 'http://localhost:4000';
+
+// const App = () => {
+//   return (
+//     <div className='flex items-start min-h-screen'>
+//       <ToastContainer />
+//       <Sidebar />
+//       <div className='flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]'>
+//         <Navbar />
+//         <div className="pt-8 pl-5 sm:pt-12 sm:pl-12">
+//           <Routes>
+//             <Route path="/add-song" element={<AddSong />} />
+//             <Route path="/list-songs" element={<ListSong />} />
+//             <Route path="/add-album" element={<AddAlbum />} />
+//             <Route path="/list-albums" element={<ListAlbum />} />
+//           </Routes>
+//         </div>
+
+//       </div>
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AddSong from "./pages/AddSong/AddSong";
+import ListSong from "./pages/ListSong/ListSong";
+import AddAlbum from "./pages/AddAlbum/AddAlbum";
+import ListAlbum from "./pages/ListAlbum/ListAlbum";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Navbar from "./components/Navbar/Navbar";
+
+export const url = import.meta.env.VITE_BASE_URL;
 
 const App = () => {
   return (
-    <div className='flex items-start min-h-screen'>
+    <div className="flex items-start min-h-screen">
       <ToastContainer />
       <Sidebar />
-      <div className='flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]'>
+
+      <div className="flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]">
         <Navbar />
+
         <div className="pt-8 pl-5 sm:pt-12 sm:pl-12">
           <Routes>
             <Route path="/add-song" element={<AddSong />} />
@@ -26,11 +65,9 @@ const App = () => {
             <Route path="/list-albums" element={<ListAlbum />} />
           </Routes>
         </div>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
